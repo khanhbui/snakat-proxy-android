a57d522d436f456592d3fc7e557bcfb4fa35c8b3
Khanh Bui <khanh.bui@punch.vn>
2022-12-04 18:19:14 +0700

    reindent

--- src/main/java/com/snakat/proxy/data/Manifest.java
+++ src/main/java/com/snakat/proxy/data/Manifest.java
@@ -3,40 +3,22 @@ package com.snakat.proxy.data;
 import com.google.gson.annotations.SerializedName;
 
 public class Manifest {
-//
-//    @SerializedName("list")
-//    private List<Item> mList = new ArrayList<>();
-//
-//    public List<Item> getList() {
-//        return mList;
-//    }
-//
-//    public static long getTotalSize(List<Manifest> list) {
-//        long total = 0;
-//        for (Manifest item : list) {
-//            total += item.mSize;
-//        }
-//        return total;
-//    }
+    @SerializedName("filename")
+    private String mFilename;
+    @SerializedName("hash")
+    private String mHash;
+    @SerializedName("size")
+    private long mSize;
 
-//    public static class Item {
-        @SerializedName("filename")
-        private String mFilename;
-        @SerializedName("hash")
-        private String mHash;
-        @SerializedName("size")
-        private long mSize;
+    public String getFilename() {
+        return mFilename;
+    }
 
-        public String getFilename() {
-            return mFilename;
-        }
+    public String getHash() {
+        return mHash;
+    }
 
-        public String getHash() {
-            return mHash;
-        }
-
-        public long getSize() {
-            return mSize;
-        }
-//    }
+    public long getSize() {
+        return mSize;
+    }
 }
